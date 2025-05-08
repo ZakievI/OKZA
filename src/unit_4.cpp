90c48080d0abbdadbd8b45fa9c3307baf345de42
The_kletka <ieremin726@gmail.com>
2025-05-08 12:36:51 +0300

    Add: bugs

--- src/unit_4.cpp
+++ src/unit_4.cpp
@@ -5,5 +5,8 @@
 std::vector<double> calculate_s(std::vector<double> s, std::vector<double> fi_s){
     std::cout<<"2";
     std::vector<double> arr(s);
+
+    unsigned int a = -1;
+
     return arr;
 };
