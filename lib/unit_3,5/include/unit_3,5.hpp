fc2b89c26cdc76007c3292951eb6b68933119420
Tagir <Tagir@DESKTOP-BAP18TS>
2025-05-21 16:25:33 +0300

    хуху

--- lib/unit_3,5/include/unit_3,5.hpp
+++ lib/unit_3,5/include/unit_3,5.hpp
@@ -11,7 +11,7 @@
 
 
 
-double newton_method(double Pi, double G, double fi1, double initial);
+double newton_method(double Pi, double G, double fi1, double initial); // initial приближение ньютона 
 double unit3(double G, double fi1, double& betta, double& C1, double& u0, double& gammaa);
 std::vector <double> unit5(std::vector<double> s, std::vector<double> V, double gammaa, std::vector <double>& gamma, std::vector <double>& sj, std::vector<double>& S1);
 
