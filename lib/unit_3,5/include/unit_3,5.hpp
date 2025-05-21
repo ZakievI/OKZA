a040d90ef7132f0e80d8a07a43187f15d1c0f39a
Tagir <Tagir@DESKTOP-BAP18TS>
2025-05-21 16:24:03 +0300

    11

---
+++ lib/unit_3,5/include/unit_3,5.hpp
@@ -0,0 +1,18 @@
+
+#pragma once
+#ifndef _UNIT4_HPP
+#define UNIT4_HPP
+#include <vector>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include "../../../resourse/fcalc.h"
+
+
+
+
+double newton_method(double Pi, double G, double fi1, double initial);
+double unit3(double G, double fi1, double& betta, double& C1, double& u0, double& gammaa);
+std::vector <double> unit5(std::vector<double> s, std::vector<double> V, double gammaa, std::vector <double>& gamma, std::vector <double>& sj, std::vector<double>& S1);
+
+#endif
